10376a693a04a219f8eb0e373a0a5a970a90b0ea
shaunarathbun-lgtm <shaunarathbun@pingidentity.com>
2025-12-11 14:25:00 -0500

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -68,7 +68,7 @@ function setCookie(name, value, days) {
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    document.cookie = name + "=" + (value || "") + expires + "; path=/; Domain=auth.ort-one-pingone.com";
     console.log(`Cookie '${name}' created!`);
 }
 
